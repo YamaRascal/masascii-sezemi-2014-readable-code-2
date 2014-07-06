c91b71a192c5a8b49507dc488999602c22d24786
YamaRascal <s0304yamatatsu@gmail.com>
2014-07-06 15:17:39 +0900

    friend commit

--- ReadableCode/src/ReadableCode/Main.java
+++ ReadableCode/src/ReadableCode/Main.java
@@ -4,11 +4,23 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) {
 		try {
+			System.out.print("読み込むファイル名を入力してください>");//
 			BufferedReader br_filename = new BufferedReader(new InputStreamReader(System.in));
 			File file = new File(br_filename.readLine());
-			
+
 			BufferedReader br_file = new BufferedReader(new FileReader(file));
-			System.out.println(br_file.readLine());
+			String recipiNAME;
+			int count =1;
+			
+			FileWriter filewriter = new FileWriter("recipi.txt");//ファイル名の入力データをここに入れたかった
+			
+			while((recipiNAME = br_file.readLine()) != null){
+
+				filewriter.write(count+":"+br_filename.readLine());
+				filewriter.close();
+				System.out.println(recipiNAME);
+			}
+
 		}catch(FileNotFoundException e) {
 			System.err.println(e);
 		}catch(IOException e) {
